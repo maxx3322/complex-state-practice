fae9c24eb1039df0a18d3f2cf749d0f01b37b1a7
maxx3322 <maxdgeers@gmail.com>
2020-02-05 16:47:07 +0800

    final-result
    finished product working with complex states in react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,71 @@
 import React, { useState } from "react";
 
-function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
+
+function App(){
+
+  const [contact, updateContact] = useState({
+    fName:"",
+    lName:"",
+    email:""
   });
 
-  return (
-    <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
-        <button>Submit</button>
-      </form>
-    </div>
+  function handleChange(event){
+    const{value,name} =event.target;
+    updateContact(prevValue =>{
+      if(name ==="fName"){
+        return{
+          fName:value,
+          lName:prevValue.lName,
+          email:prevValue.email
+        };
+      }else if(name === "lName"){
+        return{
+          fName:prevValue.fName,
+          lName:value,
+          email:prevValue.email
+        };
+      }else if (name === "email"){
+        return{
+          fName:prevValue.fName,
+          lName:prevValue.lName,
+          email:value
+        };
+      }
+    }
+
+    )
+  }
+
+  return(
+<div className="container">
+  <h1>Hello {contact.fName} {contact.lName} </h1>
+  <p>{contact.email}</p>
+<form>
+<input 
+placeholder="first Name"
+name= "fName"
+value={contact.fName}
+onChange={handleChange}
+/>
+<input
+placeholder = "last Name"
+name="lName"
+value={contact.lName}
+onChange={handleChange}
+/>
+<input 
+placeholder = "email"
+name="email"
+value={contact.email}
+onChange={handleChange}
+
+/>
+<button>Submit</button>
+</form>
+
+
+
+</div>
   );
 }
 
